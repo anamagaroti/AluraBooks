969001c1e53f6c8a0b18a4177402c305c712664d
Ana julia magaroti <ficava123@gmail.com>
2022-12-29 10:38:59 -0300

    projeto simples finalizado

---
+++ src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,7 @@
+import livro from '../../img/livro.png'
+
+export const livros = [
+    {nome: 'Liderança em design', id: 1, src:livro},
+    {nome: 'javascript assertivo', id: 2, src:livro},
+    {nome: 'Cangaceiro em javascript', id: 3, src:livro},
+]
